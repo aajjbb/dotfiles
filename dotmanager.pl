753862cadfac6935dfba500d280c1d45020e0130
Jeferson Siqueira <jefersonlsiq@gmail.com>
2018-12-26 13:48:58 -0200

    update dotmanager file, file list and remove trailing whitespaces

--- dotmanager.pl
+++ dotmanager.pl
@@ -5,32 +5,36 @@ use warnings;
 
 use feature 'say';
 
-my $root = $ENV{'HOME'}; 
+my $root = $ENV{'HOME'};
 my $current_dir = $ENV{'PWD'};
 
 my %files = (
-    "rc.lua"        => $root . "/.config/awesome",
-    "xrandr.lua"    => $root . "/.config/awesome",
-    "wallpapers"    => $root . "/.config/awesome",
-    
-    #weechat
-    "plugins.conf"  => $root . "/.weechat",
-    "irc.conf"      => $root . "/.weechat", 
-    
-    #general
-    ".bash_profile" => $root,
-    ".emacs"        => $root,
-    ".gitconfig"    => $root,
-    ".tmux.conf"    => $root,
-    ".vimrc"        => $root,
-    ".xbindkeysrc"  => $root,	
-    ".Xresources"   => $root,
-    ".compton.conf" => $root,
-    
-    ".zshrc"        => $root,
-        
-    ".lock.sh"      => $root,
-    ".mysuspend.sh" => $root,	
+    # awesome-wm
+    # awesome depends on other packages:
+    # (awesome-copycats, awesome-revelation, freedesktop)
+    ".config/awesome/rc.lua"     => $root,
+    ".config/awesome/xrandr.lua" => $root,
+
+    # i3 config
+    ".config/i3/config"          => $root,
+
+    # general
+    ".bash_profile"              => $root,
+    ".bashrc"                    => $root,
+    ".compton.conf"              => $root,
+    ".emacs.bkp"                 => $root,
+    ".gitconfig"                 => $root,
+    ".profile"                   => $root,
+    ".spacemacs"                 => $root,
+    ".tmux.conf"                 => $root,
+    ".vimrc"                     => $root,
+    ".xbindkeysrc"               => $root,
+    ".Xresources"                => $root,
+    ".zshrc"                     => $root,
+
+    # scripts
+    ".bin/lock.sh"               => $root,
+    ".bin/suspend.sh"            => $root,
 );
 
 =doc
@@ -51,14 +55,14 @@ sub prompt {
 
 sub install {
     my $skip = shift;
-    
+
     print("Starting the Installing Process \n");
 
     foreach my $key (keys %files) {
         my $source_file = $current_dir . "/" . $key;
         my $destiny_file = $files{$key} . "/" . $key;
 
-        if (not $skip) {        
+        if (not $skip) {
             my $answer = prompt("Are you sure you want to install $key ? [y/n]");
 
             if (not ($answer eq 'y' || $answer eq 'Y')) {
@@ -66,14 +70,14 @@ sub install {
                 continue;
             }
         }
-        
+
         print("Installing " . $key . " in " . $destiny_file . "\n");
-        
+
         my $result = `cp -TR $source_file $destiny_file`;
-        
+
         if ($result) {
             say "Failed to install " . $key . " is probably being used by other application or it does not exists";
-        }    
+        }
     }
 }
 
@@ -89,7 +93,7 @@ sub update {
         my $source_file = $files{$key} . "/" . $key;
         my $destiny_file = $current_dir . "/" . $key;
 
-        if (not $skip) {            
+        if (not $skip) {
             my $answer = prompt("Are you sure you want to upload $key ? [y/n]");
 
             if (not ($answer eq 'y' || $answer eq 'Y')) {
@@ -98,9 +102,9 @@ sub update {
             }
         }
         print("Uploading " . $source_file . " in " . $destiny_file . "\n");
-        
+
         my $result = `cp -TR $source_file $destiny_file`;
-        
+
         if ($result) {
             print("Failed to update " . $key . " is probably being used by other application  or it does not exists\n");
         }
@@ -108,22 +112,22 @@ sub update {
 }
 
 my $usage = "Usage: perl dotmanager.pl [OPTION] [BYPASS]\n
-  Options: 
-    --install [Install current files in repository to system]\n
-    --update [Update files in repository with ones from the system]\n
+  Options:
+    install [Install current files in repository to system]\n
+    update [Update files in repository with ones from the system]\n
   Bypass:
     --skip [Skip Confirmations]\n
     ";
-    
+
 if ($#ARGV == 0 || $#ARGV > 2) {
     print($usage);
 } else {
     my $option = $ARGV[0];
     my $bypass = ($ARGV[1] and ($ARGV[1] eq "--skip")) or 0;
-    
-    if ($option eq "--install") {
+
+    if ($option eq "install") {
         install($bypass);
-    } elsif ($option eq "--update") {
+    } elsif ($option eq "update") {
         update($bypass);
     } else {
         print("Invalid argument\n$usage");
