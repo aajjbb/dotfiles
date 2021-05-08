6b2a4566ba8b08052ccc8512d44076b8377f0441
Jeferson Siqueira <jefersonlsiq@gmail.com>
2021-05-08 19:03:58 +0100

    adding (use-package all-the-icons) to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -70,6 +70,8 @@
     )
   )
 
+(use-package all-the-icons)
+
 ;; includes powerline
 (use-package powerline
   :ensure
@@ -97,7 +99,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(powerline smartparens smartparens-config rainbow-mode company use-package)))
+   '(all-the-icons powerline smartparens smartparens-config rainbow-mode company use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
